5f51e083fd9cba28d34792362e285d02458c7d13
Dokyun Lim <im.koremp@gmail.com>
2022-10-09 10:30:07 +0900

    feat: add filter to findUserList api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,22 @@
 import data from './data/users.json' assert { type: "json"};
 
-export const findUserList = (nameText) => {
-  return data;
+export const findUserList = (text) => {
+  if (text.length === 1) {
+    return data;
+  }
+
+  const keyword = text.slice(1);
+  const uKeyword = keyword.toUpperCase();
+
+  if (text[0] === '@') {
+    return data.filter(user => user.username.toUpperCase().includes(uKeyword)
+      || (user.personalInfo.firstName && user.personalInfo.firstName.toUpperCase().includes(uKeyword))
+      || (user.personalInfo.lastName && user.personalInfo.lastName.toUpperCase().includes(uKeyword)));
+  }
+
+  if (text[0] === '#') {
+    return data.filter(user => user.tags.includes(tag => tag.toUpperCase().includes(keyword)));
+  }
 };
 
 // todo: remove this
